1b63468e735cb7715ee813cfc232c0a271482bb6
toni2110 <92384247+toni2110@users.noreply.github.com>
2022-10-23 13:28:04 +0200

    Update IAT-disability.js

--- IAT-disability.js
+++ IAT-disability.js
@@ -6,9 +6,9 @@ var posWords = API.shuffle([
             'Sårad', 'Smärta', 'Ondskefull', 'Död', 'Misär', 'Mord']);
 	return iatExtension({
 		category1 : {
-			name : 'Rörelsehindrade Personer', //Will appear in the data.
+			name : 'Funktionshindrad', //Will appear in the data.
 			title : {
-				media : {word : 'Rörelsehindrade Personer'}, //Name of the category presented in the task.
+				media : {word : 'Funktionshindrad'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
@@ -22,9 +22,9 @@ var posWords = API.shuffle([
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
 		category2 :	{
-			name : 'Ej Rörelsehindrade Personer', //Will appear in the data.
+			name : 'Ej Funktionshindrad', //Will appear in the data.
 			title : {
-				media : {word : 'Ej Rörelsehindrade Personer'}, //Name of the category presented in the task.
+				media : {word : 'Ej Funktionshindrad'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
